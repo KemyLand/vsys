ab317d86c245761ccaa42338ad5c693e424c9042
Alejandro Soto <alejandrosotochacon@gmail.com>
2016-10-11 11:42:23 -0600

    Undo v2_0

--- versions/v1_0/curl.php
+++ versions/v1_0/curl.php
@@ -7,14 +7,7 @@ function do_curl
 	$url
 )
 {
-	$handler = curl_init();
-	curl_setopt( $handler, CURLOPT_URL, $url );
-	curl_setopt( $handler, CURLOPT_RETURNTRANSFER, TRUE );
-
-	$output = curl_exec( $handler );
-	curl_close( $handler );
-
-	return $output;
+	return file_get_contents( $url );
 }
 
 ?>
--- versions/v2_0/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once( 'utilities.php' );
-
-require_login();
-redirect_main();
-
-?>
